7c356cba4b826d206c7cc76290a0523a9121b4f2
Linda van der Pal <linda.van.der.pal@finalist.nl>
2017-06-15 09:41:34 +0200

    LAM-3, LAM-4 Added API for adding and deleting custom fields

--- nl.finalist.liferay.lam.api/src/main/java/nl/finalist/liferay/lam/api/CustomFields.java
+++ nl.finalist.liferay.lam.api/src/main/java/nl/finalist/liferay/lam/api/CustomFields.java
@@ -1,17 +1,102 @@
 package nl.finalist.liferay.lam.api;
 
-import com.liferay.expando.kernel.service.ExpandoValueLocalService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.expando.kernel.exception.NoSuchTableException;
+import com.liferay.expando.kernel.model.ExpandoColumn;
+import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
+import com.liferay.expando.kernel.service.ExpandoTableLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
 
 @Component(immediate = true)
 public class CustomFields {
 
     @Reference
-    private ExpandoValueLocalService evls;
+    private ExpandoTableLocalService tableService;
 
-    // TODO: proper method name, proper arguments, javadoc, etc....
-    public void createCustomField() {
-        evls.addExpandoValue(null);
+    @Reference
+    private ExpandoColumnLocalService columnService;
+
+    private static final Log LOG = LogFactoryUtil.getLog(CustomFields.class);
+     
+    
+     
+    public void addCustomTextField(long companyId, String entityName, String fieldName, String value) {
+        LOG.info(String.format("Start adding custom field %s for company %d to entity %s with default value %s", fieldName, companyId, entityName, value));
+     
+        ExpandoTable expandoTable = getOrAddExpandoTable(companyId, entityName, ExpandoTableConstants.DEFAULT_TABLE_NAME);
+        LOG.info("Expando Table ID : " + expandoTable.getTableId());
+     
+        ExpandoColumn expandoColumn = getOrAddExpandoColumn(companyId, entityName, ExpandoTableConstants.DEFAULT_TABLE_NAME, fieldName, expandoTable);
+        expandoColumn.setDefaultData(value);
+        LOG.info("Expando Column ID : " + expandoColumn.getColumnId());
+     
+        LOG.info("Done adding custom field");
+    }
+     
+    public void deleteCustomTextField(long companyId, String entityName, String fieldName) {
+        LOG.info(String.format("Start deleting custom field %s for company %d of entity %s ", fieldName, companyId, entityName));
+     
+        try {
+    		ExpandoTable expandoTable = tableService.getDefaultTable(companyId, entityName);
+    		
+    		ExpandoColumn exandoColumn = columnService.getColumn(companyId, entityName, ExpandoTableConstants.DEFAULT_TABLE_NAME, fieldName);
+            if (exandoColumn != null) {
+                columnService.deleteExpandoColumn(exandoColumn);
+            } else {
+            	LOG.info("No column to delete");
+            }
+            
+    		tableService.deleteExpandoTable(expandoTable);
+    	} catch (NoSuchTableException e) {
+    		LOG.info("No table to delete");
+    	} catch (PortalException e) {
+			LOG.error(e);
+		} 
+     
+        LOG.info("Done deleting custom field");
+    }
+     
+    
+    public ExpandoTable getOrAddExpandoTable(long companyId, String className, String tableName) {
+        ExpandoTable expandoTable = null;
+        try {
+            expandoTable = tableService.getDefaultTable(companyId, className);
+        } catch (NoSuchTableException e) {
+        	LOG.info("The table did not yet exist");
+            try {
+                expandoTable = tableService.addTable(companyId, className, tableName);
+            } catch (Exception e1) {
+                LOG.error(e);
+            }
+        } catch (Exception e) {
+            LOG.error(e);
+        }
+        return expandoTable;
+    }
+    
+     
+    public ExpandoColumn getOrAddExpandoColumn(long companyId, String className, String tableName, String columnName,
+            ExpandoTable expandoTable) {
+        ExpandoColumn exandoColumn = null;
+        try {
+            exandoColumn = columnService.getColumn(companyId, className, tableName, columnName);
+            if (exandoColumn == null) {
+                exandoColumn = columnService.addColumn(expandoTable.getTableId(), columnName,
+                        ExpandoColumnConstants.STRING, StringPool.BLANK);
+            }
+        } catch (SystemException | PortalException e) {
+            LOG.error(e);
+        }       
+     
+        return exandoColumn;
     }
 }
--- nl.finalist.liferay.lam.api/src/test/java/nl/finalist/liferay/lam/api/CustomFieldsTest.java
+++ nl.finalist.liferay.lam.api/src/test/java/nl/finalist/liferay/lam/api/CustomFieldsTest.java
@@ -1,32 +1,77 @@
 package nl.finalist.liferay.lam.api;
 
 
-import com.liferay.expando.kernel.service.ExpandoValueLocalService;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
+import com.liferay.expando.kernel.exception.NoSuchTableException;
+import com.liferay.expando.kernel.model.ExpandoColumn;
+import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
+import com.liferay.expando.kernel.service.ExpandoTableLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.StringPool;
 
 public class CustomFieldsTest {
 
+    private static final String FIELD_NAME = "TestField";
+	private static final long COMPANY_ID = 1L;
+	private static final String ENTITY_NAME = User.class.getName();
+	@Mock
+    private ExpandoTableLocalService tableService;
     @Mock
-    private ExpandoValueLocalService evls;
+    private ExpandoColumnLocalService columnService;
 
+    @Mock
+    private ExpandoTable mockTable;
+    @Mock
+    private ExpandoColumn mockColumn;
+    
     @InjectMocks
     private CustomFields customFields;
+    
 
     @Before
-    public void setUp() {
+    public void setUp() throws PortalException {
         customFields = new CustomFields();
         initMocks(this);
     }
 
     @Test
-    public void test() {
-        customFields.createCustomField();
-        verify(evls).addExpandoValue(null);
+    public void testAddCustomTextField() throws PortalException {
+        when(tableService.getDefaultTable(COMPANY_ID, ENTITY_NAME)).thenThrow(new NoSuchTableException());
+        when(tableService.addTable(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME)).thenReturn(mockTable);
+        when(mockTable.getTableId()).thenReturn(1L);
+        when(columnService.getColumn(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME, FIELD_NAME)).thenReturn(null);
+        when(columnService.addColumn(1L, FIELD_NAME, ExpandoColumnConstants.STRING, StringPool.BLANK)).thenReturn(mockColumn);
+    	
+        customFields.addCustomTextField(COMPANY_ID, ENTITY_NAME, FIELD_NAME, "default");
+        
+        verify(tableService).getDefaultTable(COMPANY_ID, ENTITY_NAME);
+        verify(tableService).addTable(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME);
+        verify(columnService).getColumn(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME, FIELD_NAME);
+        verify(columnService).addColumn(1L, FIELD_NAME, ExpandoColumnConstants.STRING, StringPool.BLANK);
+    }
+    
+    @Test
+    public void testDeleteCustomTextField() throws PortalException {
+    	when(tableService.getDefaultTable(COMPANY_ID, ENTITY_NAME)).thenReturn(mockTable);
+    	when(columnService.getColumn(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME, FIELD_NAME)).thenReturn(mockColumn);
+    	
+        customFields.deleteCustomTextField(COMPANY_ID, ENTITY_NAME, FIELD_NAME);
+        
+        verify(tableService).getDefaultTable(COMPANY_ID, ENTITY_NAME);
+        verify(columnService).getColumn(COMPANY_ID, ENTITY_NAME, ExpandoTableConstants.DEFAULT_TABLE_NAME, FIELD_NAME);
+        verify(columnService).deleteExpandoColumn(mockColumn);
+        verify(tableService).deleteExpandoTable(mockTable);
     }
 }
