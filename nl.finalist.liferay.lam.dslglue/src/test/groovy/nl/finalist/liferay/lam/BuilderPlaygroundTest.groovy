9beab45711906094d290564932c520ddf030981b
Geert van der Ploeg <geert.van.der.ploeg@finalist.nl>
2017-07-12 11:15:46 +0200

    Added a builder playgroun test, to showcase a possible structure for embedded custom field (instances) (test fails though...)

---
+++ nl.finalist.liferay.lam.dslglue/src/test/groovy/nl/finalist/liferay/lam/BuilderPlaygroundTest.groovy
@@ -0,0 +1,108 @@
+package nl.finalist.liferay.lam
+
+import groovy.transform.ToString
+import org.junit.Test
+/*
+
+This is a playground / PoC to see the nested builder structure in action.
+It lets you create Users, and within those, create custom field (instances).
+Potentially, these CustomFieldInstances can also be nested into other
+entities, like they can in normal Liferay too.
+
+Below, all classes are defined in one go, to have a self contained example. If
+we adopt this, it should of course be expanded into proper separate classes/files.
+ */
+
+
+class BuilderPlaygroundTest {
+
+    @ToString
+    class CustomFieldInstance {
+        String name
+        Object value
+    }
+
+    @ToString
+    class User {
+        String screenName
+        String firstName
+        String emailAddress
+
+        List<CustomFieldInstance> customFields = new ArrayList<>()
+
+        void addCustomFieldInstance(CustomFieldInstance customFieldInstance) {
+            customFields += customFieldInstance
+        }
+    }
+
+    class CreateUserFactory extends AbstractFactory {
+
+        @Override
+        Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) throws InstantiationException, IllegalAccessException {
+            new User(attributes)
+        }
+
+        @Override
+        void onNodeCompleted(FactoryBuilderSupport builder, Object parent, Object node) {
+            println "About to create user with custom fields: " + node
+        }
+    }
+
+    class CreateCustomFieldInstanceFactory extends AbstractFactory {
+
+        @Override
+        Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) throws InstantiationException, IllegalAccessException {
+            new CustomFieldInstance(attributes)
+        }
+
+        @Override
+        boolean isLeaf() {
+            return true
+        }
+
+        @Override
+        void setParent(FactoryBuilderSupport builder, Object parent, Object child) {
+
+            if (parent instanceof User) {
+                parent.addCustomFieldInstance(child)
+            }
+        }
+    }
+
+
+    class PlaygroundCreateFactoryBuilder extends FactoryBuilderSupport {
+
+
+
+        void registerFactories() {
+            registerFactory("user", new CreateUserFactory());
+            registerFactory("customFieldInstance", new CreateCustomFieldInstanceFactory());
+        }
+
+
+    }
+
+
+    @Test
+    void testDSL() {
+
+        // Would be fed to the DslExecutor's binding
+        def create = new PlaygroundCreateFactoryBuilder()
+
+        // The actual resultant DSL:
+        create.user (
+                firstName: 'Geert',
+                screenName: 'geertpl',
+                emailAddress: 'geert.van.der.ploeg@finalist.nl'
+        ) {
+            customFieldInstance name: 'field1', value: 'theValue1'
+            customFieldInstance name: 'field2', value: 'theValue2'
+            customFieldInstance name: 'field3', value: 'theValue3'
+            customFieldInstance name: 'field4', value: 'theValue4'
+        }
+
+
+    }
+
+
+}
