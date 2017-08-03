9cbae9da4ce81ab7f15e6beaec74f37b03a410a7
Linda van der Pal <linda.van.der.pal@finalist.nl>
2017-08-03 15:38:25 +0200

    Corrected template name

--- nl.finalist.liferay.lam.dslglue/src/main/groovy/nl/finalist/liferay/lam/dslglue/Templates.groovy
+++ nl.finalist.liferay.lam.dslglue/src/main/groovy/nl/finalist/liferay/lam/dslglue/Templates.groovy
@@ -8,7 +8,7 @@ class Templates {
     static final two_columns_70_30 = "layout-template-id=2_columns_iii";
     static final three_columns = "layout-template-id=3_columns"
     static final one_two_columns_30_70 = "layout-template-id=1_2_columns_i";
-    static final one_two_columns_50_50 = "layout-template-id=1_2_columns_ii";
+    static final one_two_columns_70_30 = "layout-template-id=1_2_columns_ii";
     static final one_two_one_columns = "layout-template-id=1_2_1_columns";
 	static final two_two_columns = "layout-template-id=2_2_columns";
 }
