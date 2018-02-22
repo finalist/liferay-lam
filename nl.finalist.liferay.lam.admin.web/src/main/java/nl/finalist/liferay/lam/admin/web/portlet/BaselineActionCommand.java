312fcc1f484f049bf0dbba71c356efaeec58e655
Geert van der Ploeg <geert.van.der.ploeg@finalist.nl>
2018-02-22 15:42:23 +0100

    Unused imports

--- nl.finalist.liferay.lam.admin.web/src/main/java/nl/finalist/liferay/lam/admin/web/portlet/BaselineActionCommand.java
+++ nl.finalist.liferay.lam.admin.web/src/main/java/nl/finalist/liferay/lam/admin/web/portlet/BaselineActionCommand.java
@@ -4,9 +4,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionMessages;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.Validator;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
