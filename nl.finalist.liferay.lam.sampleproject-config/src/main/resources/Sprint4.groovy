7d4f43cd36dbf7c45d917c181b236a556b03bbf1
Linda van der Pal <linda.van.der.pal@finalist.nl>
2018-03-15 11:58:25 +0100

    LAM-77 Altered already existing scripts so running the config from scratch will work (mind you that if you had run the scripts before you will have to empty the LAM-changelog table)

--- nl.finalist.liferay.lam.sampleproject-config/src/main/resources/Sprint4.groovy
+++ nl.finalist.liferay.lam.sampleproject-config/src/main/resources/Sprint4.groovy
@@ -1,7 +1,7 @@
 createOrUpdate.page( 
         siteKey: "Fictional Bank",
         privatePage: true,
-        nameMap: ["nl_NL": "Verborgen pagina", "en_US": "Hidden page"],
+        nameMap: ["nl_NL": "Verborgen pagina (NL)", "en_US": "Hidden page (US)"],
         titleMap: ["nl_NL": "Titel verborgen pagina"],
         descriptionMap: ["nl_NL": "Omschrijving verborgen pagina"],
         friendlyUrlMap: ["nl_NL": "/verborgenpagina", "en_US": "/hiddenpage"],
